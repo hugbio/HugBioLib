3ffc57412309e8e9fd9104a32091d171409bba60
hugbio <847770680@qq.com>
2016-09-01 11:14:46 +0800

    文件工具类新增对象的存储和读取功能。

--- src/main/java/com/hugbio/utils/FileHelper.java
+++ src/main/java/com/hugbio/utils/FileHelper.java
@@ -1,17 +1,59 @@
 package com.hugbio.utils;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
-import android.graphics.Bitmap;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.List;
 
 public class FileHelper {
-	
-	public static FileOutputStream createFileOutputStream(String strPath) throws Exception{
+
+
+	/**
+	 * 获取可以使用的缓存目录
+	 *
+	 * @param context
+	 * @param uniqueName
+	 *            目录名称
+	 * @return
+	 */
+	public static File getDiskCacheDir(Context context, String uniqueName) {
+		final String cachePath = Environment.MEDIA_MOUNTED.equals(Environment
+				.getExternalStorageState()) ? getExternalCacheDir(context)
+				.getPath() : context.getCacheDir().getPath();
+
+		return new File(cachePath + File.separator + uniqueName);
+	}
+
+	/**
+	 * 获取程序外部的缓存目录
+	 *
+	 * @param context
+	 * @return
+	 */
+	public static File getExternalCacheDir(Context context) {
+		final String cacheDir = "/Android/data/" + context.getPackageName()
+				+ "/cache/";
+		return new File(Environment.getExternalStorageDirectory().getPath()
+				+ cacheDir);
+	}
+
+
+
+	public static FileOutputStream createFileOutputStream(String strPath) throws Exception {
 		final File file = new File(strPath);
 		try{
 			return new FileOutputStream(file);
@@ -88,11 +130,11 @@ public class FileHelper {
 	}
 
 	
-	public static void saveBitmapToFile(String pathDst,Bitmap bmp){
+	public static void saveBitmapToFile(String pathDst, Bitmap bmp){
 		saveBitmapToFile(pathDst, bmp, 80);
 	}
 	
-	public static void saveBitmapToFile(String pathDst,Bitmap bmp,int quality){
+	public static void saveBitmapToFile(String pathDst, Bitmap bmp, int quality){
 		try {
 			FileOutputStream fos = createFileOutputStream(pathDst);
 			bmp.compress(Bitmap.CompressFormat.JPEG, quality, fos);
@@ -101,7 +143,7 @@ public class FileHelper {
 		}
 	}
 	
-	public static void copyFile(String strPathDst,String strPathSrc){
+	public static void copyFile(String strPathDst, String strPathSrc){
 		if(strPathDst != null && !strPathDst.equals(strPathSrc)){
 			FileOutputStream fos = null;
 			FileInputStream fis = null;
@@ -159,7 +201,7 @@ public class FileHelper {
 		return null;
 	}
 	
-	public static String getFileExt(String fileName,String def){
+	public static String getFileExt(String fileName, String def){
 		if(fileName == null){
 			return def;
 		}
@@ -170,7 +212,7 @@ public class FileHelper {
 		return def;
 	}
 
-	public static boolean rename(String src,String dst,boolean isCover) {
+	public static boolean rename(String src, String dst, boolean isCover) {
 		if (!src.equals(dst)) {
 			File newFile = new File(dst);
 			File oldfile = new File(src);
@@ -188,4 +230,292 @@ public class FileHelper {
 		}
 		return true;
 	}
+
+	//----------------------------------------------2016.9.1新增-----------------------------------------------------
+	/**
+	 * 存储单个Parcelable对象
+	 *
+	 * @param fileName
+	 *            保存路径
+	 * @param parcelObject
+	 *            对象必须实现Parcelable
+	 */
+	public static boolean writeParcelable(String fileName, Parcelable parcelObject)
+	{
+		boolean success = false;
+		FileOutputStream fos = null;
+		try
+		{
+			fos = createFileOutputStream(fileName);
+			Parcel parcel = Parcel.obtain();
+			parcel.writeParcelable(parcelObject, Parcelable.PARCELABLE_WRITE_RETURN_VALUE);
+			byte[] data = parcel.marshall();
+			fos.write(data);
+
+			success = true;
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			if (fos != null)
+			{
+				try
+				{
+					fos.close();
+				}
+				catch (IOException ioe)
+				{
+					ioe.printStackTrace();
+				}
+			}
+		}
+
+		return success;
+	}
+
+	/**
+	 * 存储List对象
+	 *
+	 * @param fileName
+	 *            保存路径
+	 * @param list
+	 *            对象数组集合，对象必须实现Parcelable
+	 */
+	public static boolean writeParcelableList(String fileName, List<Parcelable> list)
+	{
+		boolean success = false;
+		FileOutputStream fos = null;
+		try
+		{
+			if (list instanceof List)
+			{
+				fos = createFileOutputStream(fileName);
+				Parcel parcel = Parcel.obtain();
+				parcel.writeList(list);
+				byte[] data = parcel.marshall();
+				fos.write(data);
+
+				success = true;
+			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			if (fos != null)
+			{
+				try
+				{
+					fos.close();
+				}
+				catch (IOException ioe)
+				{
+					ioe.printStackTrace();
+				}
+			}
+		}
+
+		return success;
+	}
+
+	/**
+	 * 读取单个数据对象
+	 *
+	 * @param fileName
+	 *            文件路径
+	 * @return Parcelable, 读取到的Parcelable对象，失败返回null
+	 */
+	public static Parcelable readParcelable(String fileName, ClassLoader classLoader)
+	{
+		Parcelable parcelable = null;
+		FileInputStream fis = null;
+		ByteArrayOutputStream bos = null;
+		try
+		{
+			fis = new FileInputStream(fileName);
+			if (fis != null)
+			{
+				bos = new ByteArrayOutputStream();
+				byte[] b = new byte[4096];
+				int bytesRead;
+				while ((bytesRead = fis.read(b)) != -1)
+				{
+					bos.write(b, 0, bytesRead);
+				}
+
+				byte[] data = bos.toByteArray();
+
+				Parcel parcel = Parcel.obtain();
+				parcel.unmarshall(data, 0, data.length);
+				parcel.setDataPosition(0);
+				parcelable = parcel.readParcelable(classLoader);
+			}
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+			parcelable = null;
+		}
+		finally
+		{
+			if (fis != null) try
+			{
+				fis.close();
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+			if (bos != null) try
+			{
+				bos.close();
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+
+		return parcelable;
+	}
+
+	/**
+	 * 读取数据对象列表
+	 *
+	 * @param fileName
+	 *            文件路径
+	 * @return List, 读取到的对象数组，失败返回null
+	 */
+	public static List<Parcelable> readParcelableList(String fileName, ClassLoader classLoader)
+	{
+		List<Parcelable> results = null;
+		FileInputStream fis = null;
+		ByteArrayOutputStream bos = null;
+		try
+		{
+			fis = new FileInputStream(fileName);
+			if (fis != null)
+			{
+				bos = new ByteArrayOutputStream();
+				byte[] b = new byte[4096];
+				int bytesRead;
+				while ((bytesRead = fis.read(b)) != -1)
+				{
+					bos.write(b, 0, bytesRead);
+				}
+
+				byte[] data = bos.toByteArray();
+
+				Parcel parcel = Parcel.obtain();
+				parcel.unmarshall(data, 0, data.length);
+				parcel.setDataPosition(0);
+				results = parcel.readArrayList(classLoader);
+			}
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+			results = null;
+		}
+		finally
+		{
+			if (fis != null) try
+			{
+				fis.close();
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+			if (bos != null) try
+			{
+				bos.close();
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+
+		return results;
+	}
+
+	public static boolean writeSerializable(String fileName, Serializable data)
+	{
+		boolean success = false;
+		ObjectOutputStream oos = null;
+		try
+		{
+			oos = new ObjectOutputStream(createFileOutputStream(fileName));
+			oos.writeObject(data);
+			success = true;
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			if (oos != null)
+			{
+				try
+				{
+					oos.close();
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+		return success;
+	}
+
+	public static Serializable readSerialLizable(String fileName)
+	{
+		Serializable data = null;
+		ObjectInputStream ois = null;
+		try
+		{
+			ois = new ObjectInputStream(new FileInputStream(fileName));
+			data = (Serializable) ois.readObject();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			if (ois != null)
+			{
+				try
+				{
+					ois.close();
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return data;
+	}
+
+
+
+
 }
